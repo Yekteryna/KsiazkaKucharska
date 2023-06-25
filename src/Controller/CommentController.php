d27649f855df9d39cd2c9cf94f5826511fe698e6
Yekteryna <128062849+Yekteryna@users.noreply.github.com>
2023-06-25 23:43:10 +0200

    finish

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Comment;
+use App\Form\CategoryType;
+use App\Form\CommentType;
+use App\Repository\CategoryRepository;
+use App\Repository\CommentRepository;
+use DateTimeImmutable;
+use JetBrains\PhpStorm\NoReturn;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/comment')]
+class CommentController extends AbstractController
+{
+    #[Route('/', name: 'app_comment_index', methods: ['GET'])]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_comment_delete', methods: ['post'])]
+    public function delete(Request $request, Comment $comment, CommentRepository $commentRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$comment->getId(), $request->request->get('_token'))) {
+            $commentRepository->remove($comment, true);
+        }
+
+        return $this->redirectToRoute('app_recipe_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
